707331fc067fc379afba0b7f39edb299acc43a77
Konrad Palczynski <20107556@setu.ie>
2026-04-29 19:10:01 +0100

    Test commit

--- ButtonTest.py
+++ ButtonTest.py
@@ -4,7 +4,9 @@ import time
 BUTTON_PIN = 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+button = GPIO.setup(BUTTON, GPIO.IN)
+
+print("System ready...")
 
 while True:
     print(GPIO.input(BUTTON_PIN))
