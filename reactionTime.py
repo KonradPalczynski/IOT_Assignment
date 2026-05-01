5e5985ceb99698822e0321e789d5550367cadcad
Konrad Palczynski <20107556@mail.wit.ie>
2026-05-01 20:36:57 +0100

    Added rating messages and send results to gauge in blynk

--- reactionTime.py
+++ reactionTime.py
@@ -25,7 +25,7 @@ def handle_v0_write(value):
     global waiting, next_time, led_on_time
     button_value = value[0]
     blynk.last_activity = time()
-    print(f'Current button value: {button_value}')
+    #print(f'Current button value: {button_value}')
 
     if button_value == "1":
         print("Button Pressed")
@@ -34,7 +34,20 @@ def handle_v0_write(value):
             led.off()
             waiting = False
             next_time = time() + random.uniform(2, 5)
+            blynk.virtual_write(2, int(reaction_ms))
             print(f"Reaction Time (ms): {reaction_ms:.0f} ms")
+            if reaction_ms < 250:
+                print("WOW! Perfect reactions")
+            elif reaction_ms < 400:
+                print("Great reflexes")
+            elif reaction_ms < 600:
+                print("Not bad")
+
+            else:
+                print("Too slow. Keep training")
+            print("Next round starting soon. Get ready...\n")
+
+
         else:
             print("Too early! Wait for LED to turn on")
             next_time = time() + random.uniform(2,5)
