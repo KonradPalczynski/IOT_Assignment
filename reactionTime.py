0103d734660348d1041c94cee86a8ebd2eb1d041
Konrad Palczynski <20107556@mail.wit.ie>
2026-05-03 17:24:17 +0100

    Track scores and show rounds played, avg and best score on exit

--- reactionTime.py
+++ reactionTime.py
@@ -7,7 +7,7 @@ BLYNK_AUTH = "eXHILSPsN8l1a3sybX44jSoMCQ3oZLeF"
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
 led = LED(5)
-
+reaction_times = []
 waiting = False
 led_on_time = 0
 next_time = time() + random.uniform(2, 5)
@@ -31,6 +31,7 @@ def handle_v0_write(value):
         print("Button Pressed")
         if waiting: 
             reaction_ms = (time() - led_on_time) *1000
+            reaction_times.append(reaction_ms)
             led.off()
             waiting = False
             blynk.virtual_write(1,0)
@@ -55,20 +56,37 @@ def handle_v0_write(value):
 
     else: 
         print("Button Released")
-
-while True:
-    blynk.run()
+try:
+    while True:
+       blynk.run()
     
-    current = time()
+       current = time()
 
-    if not waiting and current >= next_time:
-        led.on()
-        waiting = True
-        led_on_time = current
-        blynk.virtual_write(1, 1)
-        print("LED ON - press button!")
+       if not waiting and current >= next_time:
+           led.on()
+           waiting = True
+           led_on_time = current
+           blynk.virtual_write(1, 1)
+           print("LED ON - press button!")
+       sleep(0.01)
+except KeyboardInterrupt:
+    led.off()
+    blynk.virtual_write(1,0)
+    print("\n GAME OVER")
+    if reaction_times:
+        avg = sum(reaction_times) / len(reaction_times)
+        best = min(reaction_times)
+        print(f"Rounds Played: {len(reaction_times)}")
+        print(f"Best Time: {best:.0f}ms")
+        print(f"Average Reaction Time: {avg:.0f}ms")
+    else:
+        print("No Rounds Played")
+    print("Thank you for playing")
+        
 
-    if not waiting and current >= next_time:
-        next_time = current + random.uniform(2, 5)
 
-    sleep(0.01)
+
+    # if not waiting and current >= next_time:
+    #     next_time = current + random.uniform(2, 5)
+
+    # sleep(0.01)
