1ec419b06a2bb756236fbc387f5646a221ec01ab
Konrad Palczynski <20107556@mail.wit.ie>
2026-05-03 19:17:04 +0100

    added json file which saves data and timestamps it

--- reactionTime.py
+++ reactionTime.py
@@ -1,4 +1,7 @@
 import BlynkLib
+import json
+import os
+from datetime import datetime
 from gpiozero import LED
 from time import sleep, time
 import random
@@ -7,14 +10,44 @@ BLYNK_AUTH = "eXHILSPsN8l1a3sybX44jSoMCQ3oZLeF"
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
 led = LED(5)
+Results_file = "ReactionResults.json" 
 reaction_times = []
 waiting = False
 led_on_time = 0
 next_time = time() + random.uniform(2, 5)
 
+def get_rating(reaction_ms):
+            if reaction_ms < 250:
+                return "WOW! Perfect reactions"
+            elif reaction_ms < 400:
+                return "Great reflexes"
+            elif reaction_ms < 600:
+                return "Not bad"
+
+            else:
+                return "Too slow. Keep training"
+
 print("REACTION TIME GAME")
 print("Press the Button when LED turns on!")
 
+def save_results(reactions_ms, rating):
+    if os.path.exists(Results_file):
+        with open(Results_file, "r") as f:
+            data = json.load(f)
+    
+    else: 
+        data = []
+
+    entry = {
+        "timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S %p"),
+        "Reaction Time": int(reactions_ms),
+        "rating": rating
+    }
+
+    data.append(entry)
+    with open(Results_file, "w") as f:
+        json.dump(data, f, indent=4)
+    print(f"Results saved: {entry}")
 
  
 
@@ -25,7 +58,6 @@ def handle_v0_write(value):
     global waiting, next_time, led_on_time
     button_value = value[0]
     blynk.last_activity = time()
-    #print(f'Current button value: {button_value}')
 
     if button_value == "1":
         print("Button Pressed")
@@ -38,15 +70,10 @@ def handle_v0_write(value):
             next_time = time() + random.uniform(2, 5)
             blynk.virtual_write(2, int(reaction_ms))
             print(f"Reaction Time (ms): {reaction_ms:.0f} ms")
-            if reaction_ms < 250:
-                print("WOW! Perfect reactions")
-            elif reaction_ms < 400:
-                print("Great reflexes")
-            elif reaction_ms < 600:
-                print("Not bad")
-
-            else:
-                print("Too slow. Keep training")
+            rating = get_rating(reaction_ms)
+            print(rating)
+            save_results(reaction_ms, rating)
+        
             print("Next round starting soon. Get ready...\n")
 
 
@@ -54,8 +81,8 @@ def handle_v0_write(value):
             print("Too early! Wait for LED to turn on")
             next_time = time() + random.uniform(2,5)
 
-    else: 
-        print("Button Released")
+    # else: 
+    #     print("Button Released")
 try:
     while True:
        blynk.run()
@@ -85,8 +112,3 @@ except KeyboardInterrupt:
         
 
 
-
-    # if not waiting and current >= next_time:
-    #     next_time = current + random.uniform(2, 5)
-
-    # sleep(0.01)
